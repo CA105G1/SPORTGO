c5078c41527f253447c634fe3ce568c5b5ce6b4f
wesley <w093578578888@gmail.com>
2018-12-13 19:59:59 +0800

    商城第一版完成(沒有購物車)

--- src/com/product/model/ProdouctJDBCDAO.java
+++ src/com/product/model/ProdouctJDBCDAO.java
@@ -10,15 +10,16 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 	private static final String USER = "test";
 	private static final String PASSWORD = "123456";
 	//新增
-	private static final String INSERT_INTO_DATA = "Insert into PRODUCT (PRO_NO,PRO_CLASSID,PRO_NAME,PRO_PIC,PRO_PIC_EXT,PRO_FORMAT,PRO_BONUS,PRO_STOCK,PRO_SAFESTOCK,PRO_DETAILS,PRO_SHELVE,PRO_ALL_ASSESS,PRO_ALL_ASSESSMAN) values ('PR'||LPAD(to_char(PRODUCT_SEQ.NEXTVAL), 3, '0'),?,?,?,?,?,?,?,?,?,?,?,?)";
+	private static final String INSERT = "Insert into PRODUCT (PRO_NO,PRO_CLASSID,PRO_NAME,PRO_PIC,PRO_PIC_EXT,PRO_FORMAT,PRO_BONUS,PRO_STOCK,PRO_SAFESTOCK,PRO_DETAILS,PRO_SHELVE,PRO_ALL_ASSESS,PRO_ALL_ASSESSMAN) values ('P'||LPAD(to_char(PRODUCT_SEQ.NEXTVAL), 3, '0'),?,?,?,?,?,?,?,?,?,?,?,?)";
 	//修改
-	private static final String INSERT_UPDATE = "UPDATE PRODUCT SET  PRO_CLASSID = ?, PRO_NAME = ?, PRO_PIC = ?, PRO_PIC_EXT = ?, PRO_FORMAT = ?, PRO_BONUS = ?, PRO_STOCK = ?, PRO_SAFESTOCK = ?, PRO_DETAILS = ?, PRO_SHELVE = ?, PRO_ALL_ASSESS = ?, PRO_ALL_ASSESSMAN = ? where PRO_NO = ?";
-	//單筆查詢、多筆查詢
-	private static final String SELETE_FINDBYPK = "select * from product where pro_no = ?";
-	private static final String SELETE_ALL = "SELECT * FROM PRODUCT";
+	private static final String UPDATE = "UPDATE PRODUCT SET  PRO_CLASSID = ?, PRO_NAME = ?, PRO_PIC = ?, PRO_PIC_EXT = ?, PRO_FORMAT = ?, PRO_BONUS = ?, PRO_STOCK = ?, PRO_SAFESTOCK = ?, PRO_DETAILS = ?, PRO_SHELVE = ?, PRO_ALL_ASSESS = ?, PRO_ALL_ASSESSMAN = ? where PRO_NO = ?";
+	//單筆查詢
+	private static final String SELECT_FINDBYPK = "select * from product where pro_no = ?";
+	//多筆查詢
+	private static final String SELECT_ALL = "SELECT * FROM PRODUCT";
 	//刪除
-	private static final String DELETE_CHILDREN_01 = "Delete FROM ORDDETAILS WHERE ORD_NO = ?";
-	private static final String DELETE_CHILDERN_02 = "Delete FROM PRO_DETAIL_PROM WHERE PROM_PROJECT_ID = ?";
+	private static final String DELETE_CHILDREN_PROM = "Delete FROM PRO_DETAIL_PROM WHERE PROM_PROJECT_ID = ?";
+	private static final String DELETE_CHILDREN_ORDDETAILS = "Delete FROM ORDDETAILS WHERE ORD_NO = ?";
 	private static final String DELETE_DATA = "Delete FROM PRODUCT WHERE PRO_NO = ?";
 	
 	
@@ -33,17 +34,15 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 	
 	
 	
-	@Override
-	public void insert(ProdouctVO proVO) {
+	@Override  //新增
+	public int insert(ProdouctVO proVO) {
 		Connection con = null;
 		PreparedStatement ps = null;
-		
+		int count = 0 ;
 		 try {
-			    
-
 				con = DriverManager.getConnection(URL, USER, PASSWORD);
 				
-		        ps = con.prepareStatement(INSERT_INTO_DATA);
+		        ps = con.prepareStatement(INSERT);
 		        ps.setString(1,proVO.getPro_classid());
 		        ps.setString(2, proVO.getPro_name());
 		        ps.setBytes(3, proVO.getPro_pic());
@@ -57,7 +56,7 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 		    	ps.setInt (11,proVO.getPro_all_assess());
 		    	ps.setInt (12,proVO.getPro_all_assessman());
 		        
-		        ps.executeQuery();
+		    	count = ps.executeUpdate();
 		        
 		    } catch (SQLException  e) {
 				// TODO Auto-generated catch block
@@ -89,20 +88,20 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 					}
 				}
 			}
-		
+		return count;
 		
 		
 	}
-
+    //更新
 	@Override
-	public void update(ProdouctVO proVO) {
+	public int update(ProdouctVO proVO) {
 		Connection con = null;
 		PreparedStatement ps = null;
-		
+		int count = 0;
 		try {
 			
 			con = DriverManager.getConnection(URL, USER, PASSWORD);
-		    ps = con.prepareStatement(INSERT_UPDATE);
+		    ps = con.prepareStatement(UPDATE);
 		    ps.setString(1,proVO.getPro_classid());
 	        ps.setString(2, proVO.getPro_name());
 	        ps.setBytes(3, proVO.getPro_pic());
@@ -117,7 +116,7 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 	    	ps.setInt (12,proVO.getPro_all_assessman());
 	    	ps.setString(13, proVO.getPro_no());
 	    	
-	    	ps.executeQuery();
+	    	count = ps.executeUpdate();
 		    
 		} catch (  SQLException e) {
 			// TODO Auto-generated catch block
@@ -140,42 +139,49 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 				}
 			}
 		}
-		
+		return count;
 		
 	}
-
+    //刪除
 	@Override
-	public void delete(String ord_no ,String prom_project_id ,  String pro_no) {
+	public int delete(String ord_no ,String prom_project_id ,  String pro_no) {
 		Connection con = null;
 		PreparedStatement ps = null;
-		ResultSet rs = null;
+		int count = 0;
+//		ResultSet rs = null;
 		try {
-			con = DriverManager.getConnection(URL, USER, PASSWORD);
-			ps = con.prepareStatement(DELETE_CHILDREN_01);
-			ps.setString(1,ord_no );
-			rs = ps.executeQuery();
-	
-			ps = con.prepareStatement(DELETE_CHILDERN_02);
-			ps.setString(1,prom_project_id );
-			rs = ps.executeQuery();
+//			ps = con.prepareStatement(DELETE_CHILDREN_PROM);
+//			ps.setString(1,prom_project_id );
+//			ps.addBatch();	
+//			
+//			con = DriverManager.getConnection(URL, USER, PASSWORD);
+//			ps = con.prepareStatement(DELETE_CHILDREN_ORDDETAILS);
+//			ps.setString(1,ord_no );
+//			ps.addBatch();	
+//			
+//			ps = con.prepareStatement(DELETE_DATA);
+//			ps.setString(1, pro_no);
+//			ps.addBatch();
+//			ps.executeBatch();
 			
+			con = DriverManager.getConnection(URL, USER, PASSWORD);
+			ps = con.prepareStatement(DELETE_CHILDREN_PROM);
+			ps.setString(1,prom_project_id );
+			ps.executeUpdate();
+			
+			ps = con.prepareStatement(DELETE_CHILDREN_ORDDETAILS);
+			ps.setString(1,ord_no );
+			ps.executeUpdate();
+
 			ps = con.prepareStatement(DELETE_DATA);
 			ps.setString(1, pro_no);
-			rs = ps.executeQuery();
+			count = ps.executeUpdate();
 			
 			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
 			if(ps != null) {
 				try {
 					ps.close();
@@ -193,37 +199,37 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 				}
 			}
 		}
-		
+		return count ;
 	}
-
+    //全部查詢
 	@Override
 	public List<ProdouctVO> getAll() {
-		List<ProdouctVO> provolist = new ArrayList<>();
-		ProdouctVO provo = null;
+		List<ProdouctVO> proVOList = new ArrayList<>();
+		ProdouctVO proVO = null;
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
 		try {
 			con = DriverManager.getConnection(URL, USER, PASSWORD);
-			ps = con.prepareStatement(SELETE_ALL);
+			ps = con.prepareStatement(SELECT_ALL);
 		    rs = ps.executeQuery();
 		    
 		    while(rs.next()) {
-		    	provo = new ProdouctVO();
-		    	provo.setPro_no(rs.getString("PRO_NO"));
-				provo.setPro_classid(rs.getString("PRO_CLASSID"));
-				provo.setPro_name(rs.getString("PRO_NAME"));
-				provo.setPro_pic(rs.getBytes("PRO_PIC"));
-				provo.setPro_pic_ext(rs.getString("PRO_PIC_EXT"));
-				provo.setPro_format(rs.getString("PRO_FORMAT"));
-				provo.setPro_bonus(rs.getInt("PRO_BONUS"));
-				provo.setPro_stock(rs.getInt("PRO_STOCK"));
-				provo.setPro_safestock(rs.getInt("PRO_SAFESTOCK"));
-				provo.setPro_details(rs.getString("PRO_DETAILS"));
-				provo.setPro_shelve(rs.getString("PRO_SHELVE"));
-				provo.setPro_all_assess(rs.getInt("PRO_ALL_ASSESS"));
-				provo.setPro_all_assessman(rs.getInt("PRO_ALL_ASSESSMAN"));
-		    	provolist.add(provo);
+		    	proVO = new ProdouctVO();
+		    	proVO.setPro_no(rs.getString("PRO_NO"));
+		    	proVO.setPro_classid(rs.getString("PRO_CLASSID"));
+		    	proVO.setPro_name(rs.getString("PRO_NAME"));
+		    	proVO.setPro_pic(rs.getBytes("PRO_PIC"));
+		    	proVO.setPro_pic_ext(rs.getString("PRO_PIC_EXT"));
+		    	proVO.setPro_format(rs.getString("PRO_FORMAT"));
+		    	proVO.setPro_bonus(rs.getInt("PRO_BONUS"));
+		    	proVO.setPro_stock(rs.getInt("PRO_STOCK"));
+		    	proVO.setPro_safestock(rs.getInt("PRO_SAFESTOCK"));
+		    	proVO.setPro_details(rs.getString("PRO_DETAILS"));
+		    	proVO.setPro_shelve(rs.getString("PRO_SHELVE"));
+		    	proVO.setPro_all_assess(rs.getInt("PRO_ALL_ASSESS"));
+		    	proVO.setPro_all_assessman(rs.getInt("PRO_ALL_ASSESSMAN"));
+		    	proVOList.add(proVO);
 		    }
 			
 		} catch (SQLException e) {
@@ -256,12 +262,12 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 			}
 		}
 		
-		return provolist;
+		return proVOList;
 	}
-
+    //單次查詢
 	@Override
 	public ProdouctVO findByPK (String pro_no) {
-		ProdouctVO provo = null;
+		ProdouctVO proVO = null;
 		Connection con = null;
 		PreparedStatement ps = null;
 		ResultSet rs = null;
@@ -269,26 +275,26 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 		try {
 			
 			con =  DriverManager.getConnection(URL, USER, PASSWORD);
-			ps = con.prepareStatement(SELETE_FINDBYPK);
+			ps = con.prepareStatement(SELECT_FINDBYPK);
 			
 			ps.setString(1, pro_no);
 			rs = ps.executeQuery();
 			
 			while (rs.next()) {
-				provo = new ProdouctVO();
-				provo.setPro_no(rs.getString("PRO_NO"));
-				provo.setPro_classid(rs.getString("PRO_CLASSID"));
-				provo.setPro_name(rs.getString("PRO_NAME"));
-				provo.setPro_pic(rs.getBytes("PRO_PIC"));
-				provo.setPro_pic_ext(rs.getString("PRO_PIC_EXT"));
-				provo.setPro_format(rs.getString("PRO_FORMAT"));
-				provo.setPro_bonus(rs.getInt("PRO_BONUS"));
-				provo.setPro_stock(rs.getInt("PRO_STOCK"));
-				provo.setPro_safestock(rs.getInt("PRO_SAFESTOCK"));
-				provo.setPro_details(rs.getString("PRO_DETAILS"));
-				provo.setPro_shelve(rs.getString("PRO_SHELVE"));
-				provo.setPro_all_assess(rs.getInt("PRO_ALL_ASSESS"));
-				provo.setPro_all_assessman(rs.getInt("PRO_ALL_ASSESSMAN"));
+				proVO = new ProdouctVO();
+				proVO.setPro_no(rs.getString("PRO_NO"));
+				proVO.setPro_classid(rs.getString("PRO_CLASSID"));
+				proVO.setPro_name(rs.getString("PRO_NAME"));
+				proVO.setPro_pic(rs.getBytes("PRO_PIC"));
+				proVO.setPro_pic_ext(rs.getString("PRO_PIC_EXT"));
+				proVO.setPro_format(rs.getString("PRO_FORMAT"));
+				proVO.setPro_bonus(rs.getInt("PRO_BONUS"));
+				proVO.setPro_stock(rs.getInt("PRO_STOCK"));
+				proVO.setPro_safestock(rs.getInt("PRO_SAFESTOCK"));
+				proVO.setPro_details(rs.getString("PRO_DETAILS"));
+				proVO.setPro_shelve(rs.getString("PRO_SHELVE"));
+				proVO.setPro_all_assess(rs.getInt("PRO_ALL_ASSESS"));
+				proVO.setPro_all_assessman(rs.getInt("PRO_ALL_ASSESSMAN"));
 				
 			}
 			
@@ -322,7 +328,7 @@ public class ProdouctJDBCDAO implements ProdouctDAO_interface{
 			}
 		}
 		
-		return provo;
+		return proVO;
 	}
 
 	
