b7abb4a6a003103dc13f7f13144dc2a476eb4a40
wenshock <wenshock@DESKTOP-GKM2KL7>
2019-01-15 15:56:16 +0800

    :han: getReg_nameList

--- src/com/region/model/RegService.java
+++ src/com/region/model/RegService.java
@@ -1,11 +1,22 @@
 package com.region.model;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class RegService {
 	private RegDAO_interface regDAO = null;
+	private List<String> reg_nameList =null;
+	
+	public List<String> getReg_nameList(){
+		if(reg_nameList==null) {
+			setReg_nameList();
+		}
+		return reg_nameList;
+	}
 	
 	public RegService() {
 		this.regDAO = new RegDAO();
@@ -21,6 +32,21 @@ public class RegService {
 	
 	public RegVO getRegVOByPK(Integer reg_no) {
 		return regDAO.findByPrimaryKey(reg_no);
+	}
+	
+	public void setReg_nameList(){
+		List<RegVO> regVOs = regDAO.getAll();
+		LinkedHashSet<String> reg_nameSet = new LinkedHashSet<>();
+		for(int i = 0 ; i<regVOs.size(); i++) {
+			String reg_name = regVOs.get(i).getReg_name();
+			if(!reg_nameSet.contains(reg_name)) {
+				reg_nameSet.add(reg_name);
+			}
+		}
+		reg_nameList = new ArrayList<>();
+		for(String reg_name : reg_nameSet) {
+			reg_nameList.add(reg_name);
+		}
 	}
 	
 	public List<RegVO> getRegVOFormRegName(String reg_name) {
