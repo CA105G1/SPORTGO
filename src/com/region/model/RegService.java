f4ebc5a34f3f3aa0ffe06da212e9a63aa28eb0a8
wenshock <wenshock@DESKTOP-GKM2KL7>
2019-01-15 15:43:26 +0800

    :Han: 地此的連動 RegService public List<RegVO> getRegVOFormRegName(String reg_name)

--- src/com/region/model/RegService.java
+++ src/com/region/model/RegService.java
@@ -1,6 +1,8 @@
 package com.region.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RegService {
 	private RegDAO_interface regDAO = null;
@@ -20,5 +22,13 @@ public class RegService {
 	public RegVO getRegVOByPK(Integer reg_no) {
 		return regDAO.findByPrimaryKey(reg_no);
 	}
+	
+	public List<RegVO> getRegVOFormRegName(String reg_name) {
+		Map<String, String[]> map = new HashMap<>();
+		String[] reg_name_array = {reg_name};
+		map.put("reg_name", reg_name_array);
+		return regDAO.getAll(map);
+	}
+	
 
 }
