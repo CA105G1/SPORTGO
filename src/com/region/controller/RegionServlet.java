9362f1ac5d626abfda8717ad890c77ec3065e66d
Shawn <w121212w00@gmail.com>
2019-01-17 20:34:11 +0800

    0117 shawn update

--- src/com/region/controller/RegionServlet.java
+++ src/com/region/controller/RegionServlet.java
@@ -1,6 +1,8 @@
 package com.region.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,6 +11,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONObject;
+
+import com.region.model.RegService;
 import com.region.model.RegVO;
 
 public class RegionServlet extends HttpServlet {
@@ -30,8 +35,46 @@ public class RegionServlet extends HttpServlet {
 //		getServletContext().setAttribute("regionMapKeyReg_name", regionMapKeyReg_name);
 //	}
 	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		res.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		PrintWriter pw = res.getWriter();
+		
+		//揪團複合查詢用
+		if("getReg_dist".equals(action)) {
+			String reg_name = req.getParameter("reg_name");
+System.out.println("reg_name="+reg_name);
+			
+			try {
+				//取得縣市的地區名稱
+				RegService regSvc = new RegService();
+				List<String> reg_distlist = regSvc.getRegVOFormRegName(reg_name);
+				StringBuffer sb = new StringBuffer();
+//				sb.append("{");
+				int count = 0;
+				for(String reg_dist : reg_distlist) {
+					if(count == 0) {
+						sb.append(reg_dist);
+						count++;						
+					}else {
+						sb.append(","+reg_dist);
+					}
+					
+				}
+//				sb.append("}");
+System.out.println("sb=" + sb.toString());
+				JSONObject obj = new JSONObject();
+				obj.put("reg_dist", sb);
+				pw.write(obj.toString());
+				
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
