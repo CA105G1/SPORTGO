50b5087494f9f717c411a4881853346402addc29
coolbee333 <coolbee333@gmail.com>
2019-01-18 06:10:06 +0800

    1/18 android update

--- src/com/android/sg_info/model/Sg_infoDAO_android.java
+++ src/com/android/sg_info/model/Sg_infoDAO_android.java
@@ -49,7 +49,7 @@ public class Sg_infoDAO_android implements Sg_infoDAO_interface_android{
 	private static final String FIND_BY_PK =
 			"SELECT SG.*, SP.sp_name, V.V_Name, M.MEM_NAME , V.V_LAT, V.V_LONG FROM sg_info SG LEFT JOIN SPORT SP on SP.sp_no = SG.sp_no LEFT JOIN VENUE V ON V.V_NO = SG.V_NO LEFT JOIN MEMBERLIST M ON SG.MEM_NO = M.MEM_NO WHERE SG.SG_NO=?";
 	private static final String FIND_BY_MASTER =
-			"SELECT SG.*, SP.sp_name, V.V_Name, M.MEM_NAME , V.V_LAT, V.V_LONG FROM sg_info SG LEFT JOIN SPORT SP on SP.sp_no = SG.sp_no LEFT JOIN VENUE V ON V.V_NO = SG.V_NO LEFT JOIN MEMBERLIST M ON SG.MEM_NO = M.MEM_NO WHERE SG.MEM_NO=? ORDER BY SG.SG_DATE DESC"; //主辦的揪團
+			"SELECT SG.*, SP.sp_name, V.V_Name, M.MEM_NAME , V.V_LAT, V.V_LONG FROM sg_info SG LEFT JOIN SPORT SP on SP.sp_no = SG.sp_no LEFT JOIN VENUE V ON V.V_NO = SG.V_NO LEFT JOIN MEMBERLIST M ON SG.MEM_NO = M.MEM_NO WHERE SG.MEM_NO=? AND SG.SG_STATUS = '揪團中' ORDER BY SG.SG_DATE DESC"; //主辦的揪團
 	private static final String FIND_BY_MEM =
 			"SELECT SG.*, SP.sp_name, V.V_Name, M.MEM_NAME , V.V_LAT, V.V_LONG FROM SG_MEM SGM LEFT JOIN SG_INFO SG ON SGM.SG_NO = SG.SG_NO LEFT JOIN SPORT SP on SP.sp_no = SG.sp_no LEFT JOIN VENUE V ON V.V_NO = SG.V_NO LEFT JOIN MEMBERLIST M ON SG.MEM_NO = M.MEM_NO WHERE SGM.MEM_NO = ? AND SG.SG_DATE > SYSDATE ORDER BY SG.SG_DATE"; //參加的揪團
 	private static final String FIND_BY_LIKE =
@@ -57,7 +57,7 @@ public class Sg_infoDAO_android implements Sg_infoDAO_interface_android{
 	private static final String FIND_BY_HIS =
 			"SELECT SG.*, SP.sp_name, V.V_Name, M.MEM_NAME , V.V_LAT, V.V_LONG FROM SG_MEM SGM LEFT JOIN SG_INFO SG ON SGM.SG_NO = SG.SG_NO LEFT JOIN SPORT SP on SP.sp_no = SG.sp_no LEFT JOIN VENUE V ON V.V_NO = SG.V_NO LEFT JOIN MEMBERLIST M ON SG.MEM_NO = M.MEM_NO WHERE SGM.MEM_NO = ? AND SG.SG_DATE < SYSDATE ORDER BY SG.SG_DATE"; //找出活動時間過的會員揪團
 	private static final String GET_ALL =
-			"SELECT SG.*, SP.sp_name, V.V_Name, M.MEM_NAME , V.V_LAT, V.V_LONG FROM sg_info SG LEFT JOIN SPORT SP on SP.sp_no = SG.sp_no LEFT JOIN VENUE V ON V.V_NO = SG.V_NO LEFT JOIN MEMBERLIST M ON SG.MEM_NO = M.MEM_NO WHERE SG.APL_END > SYSDATE ORDER BY SG_DATE"; //找出所有揪團
+			"SELECT SG.*, SP.sp_name, V.V_Name, M.MEM_NAME , V.V_LAT, V.V_LONG FROM sg_info SG LEFT JOIN SPORT SP on SP.sp_no = SG.sp_no LEFT JOIN VENUE V ON V.V_NO = SG.V_NO LEFT JOIN MEMBERLIST M ON SG.MEM_NO = M.MEM_NO WHERE SG.APL_END > SYSDATE ORDER BY SG.APL_END"; //找出所有揪團
 	private static final String GET_IMAGE =
 			"SELECT SG_PIC FROM SG_INFO WHERE SG_NO = ?";
 	
@@ -714,7 +714,7 @@ public class Sg_infoDAO_android implements Sg_infoDAO_interface_android{
 		String finalSQL = "SELECT SG.*, SP.sp_name, V.V_Name, M.MEM_NAME , V.V_LAT, V.V_LONG FROM sg_info SG LEFT JOIN SPORT SP on SP.sp_no = SG.sp_no LEFT JOIN VENUE V ON V.V_NO = SG.V_NO LEFT JOIN MEMBERLIST M ON SG.MEM_NO = M.MEM_NO where "
 		          + get_WhereCondition(map)
 		          + time
-		          + "SG.SG_STATUS = '揪團中' order by sg.Sg_no";
+		          + "SG.APL_END > SYSDATE ORDER BY SG.APL_END";
 		
 		System.out.println(finalSQL);
 		
